440207d9930097c3c39df70cf1e1200cd652db76
Sam Yerkes <samuelyerkes@gmail.com>
2020-10-08 22:36:31 -0400

    Added content type list command.

---
+++ app/Commands/ContentTypes/ContentTypesList.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Commands\ContentTypes;
+
+use LaravelZero\Framework\Commands\Command as Command;
+use App\Traits\Customizable;
+use App\Traits\T4able;
+
+class ContentTypesList extends Command
+{
+    use Customizable, T4able;
+    
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'contenttype:list
+                            {--fields=id,name : Instead of returning the whole content type, returns the value of a specified field.}
+                            {--filter= : Instead of returning all content types, returns the content types who only match a specific filter.}
+                            {--format=table}
+                            {--sort=id}
+                            {--order=desc}';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'List content types';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $url = '/contenttype';
+        $data = $this->sendRequest($url);
+
+        $fields = $this->fields($this->option('fields'));
+
+        $filter = $this->filter($this->option('filter'));
+        
+        $format = $this->option('format');
+
+        $data = $this->getFilteredContent($data, $filter);
+        
+        $data = $this->getFieldsOfContent($data, $fields);
+        
+        $sortField = $this->option('sort');
+
+        $sortOrder = $this->option('order');
+
+        $data = $this->sortContent($data, $sortField, $sortOrder);
+
+        $this->printWithFormatter($data, $format);
+
+    }
+
+}
