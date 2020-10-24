25a868ffa56490b506a00b75f31caa36281e96c7
Sam Yerkes <samuelyerkes@gmail.com>
2020-10-24 08:57:11 -0400

    Added transfer to use getOptions() method.

--- app/Commands/Transfers/TransferGet.php
+++ app/Commands/Transfers/TransferGet.php
@@ -12,7 +12,7 @@ class TransferGet extends Command
      *
      * @var string
      */
-    protected $signature = 'transfer:get {transfers?*}
+    protected $signature = 'transfer:get {details?*}
                             {--fields=id,name,remoteHost,remoteRoot,channelID : Instead of returning the whole transfer, returns the value of a specified field. (optional)}
                             {--filter= : Instead of returning all transfers, returns the transfers who only match a specific filter. (optional)}
                             {--format=table}
@@ -35,29 +35,23 @@ class TransferGet extends Command
     public function handle()
     {
         // Arguments and options
-        $labels = $this->option('labels');
-        $transfers = $this->argument('transfers');
-        $sortField = $this->option('sort');
-        $sortOrder = $this->option('order');
-        $format = $this->option('format');
-        $fields = $this->fields($this->option('fields'));
-        $filter = $this->filter($this->option('filter'));
+        $this->getOptions();
 
         // Get the details of keys passed into the command
-        $data = $this->getDetails('transfer', $transfers);
+        $data = $this->getDetails('transfer', $this->details);
 
         if (count($data)) {
 
             // If the command has the label flag then just do an early return. 
-            if ($labels) return $this->printLabels($data);
+            if ($this->labels) return $this->printLabels($data);
         
-            $data = $this->getFilteredContent($data, $filter);
+            $data = $this->getFilteredContent($data, $this->filters);
             
-            $data = $this->getFieldsOfContent($data, $fields);
+            $data = $this->getFieldsOfContent($data, $this->fields);
     
-            $data = $this->sortContent($data, $sortField, $sortOrder);
+            $data = $this->sortContent($data, $this->sort, $this->order);
     
-            $this->printWithFormatter($data, $format);
+            $this->printWithFormatter($data, $this->format);
         }
 
     }
