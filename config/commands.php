54629e11ce926c83054863cafd5b6cd5e8307c64
Sam Yerkes <samuelyerkes@gmail.com>
2020-10-27 16:57:19 -0400

    Removed default commands that are not t4 specific.

--- config/commands.php
+++ config/commands.php
@@ -74,7 +74,23 @@ return [
     */
 
     'remove' => [
-        // ..
+        Illuminate\Database\Console\Migrations\FreshCommand::class,
+        Illuminate\Database\Console\Migrations\InstallCommand::class,
+        Illuminate\Database\Console\Migrations\MigrateCommand::class,
+        Illuminate\Database\Console\Migrations\RefreshCommand::class,
+        Illuminate\Database\Console\Migrations\ResetCommand::class,
+        Illuminate\Database\Console\Migrations\RollbackCommand::class,
+        Illuminate\Database\Console\Migrations\StatusCommand::class,
+        Illuminate\Database\Console\Migrations\MigrateMakeCommand::class,
+        Illuminate\Database\Console\Seeds\SeedCommand::class,
+        Illuminate\Database\Console\WipeCommand::class,
+        Illuminate\Database\Console\Factories\FactoryMakeCommand::class,
+        Illuminate\Foundation\Console\ModelMakeCommand::class,
+        Illuminate\Database\Console\Seeds\SeederMakeCommand::class,
+        LaravelZero\Framework\Commands\MakeCommand::class,
+        LaravelZero\Framework\Commands\RenameCommand::class,
+        LaravelZero\Framework\Commands\StubPublishCommand::class,
+        LaravelZero\Framework\Commands\InstallCommand::class,
     ],
 
 ];
